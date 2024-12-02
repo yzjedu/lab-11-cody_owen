d363f59355de09624c043f5ded59f23560f43fe6
codyking <csking2@loyola.edu>
2024-12-02 13:42:58 -0500

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# Programmers:  Owen & Cody
+# Course:  CS151, Dr. Yalew
+# Due Date: 12/2
+# Programming Assignment:  Lab 11
+# Problem Statement: translates morsecode to english, then reads files in morsecode and translates to a different file
+# Data In: file names
+# Data Out:  morsecode translations
+# Credits: class reference for dictionary function, notes
+
+
 import os
 
+# Name: dictionary
+# Parameters: filename
+# Return: morse_dictionary
+# translates morsecode to english
 def dictionary(filename):
     morse_dictionary = {}
     morse_data = open(filename, 'r')
@@ -11,15 +25,22 @@ def dictionary(filename):
     morse_data.close()
     return morse_dictionary
 
+# Name: translate_morse
+# Parameters: translation, input, output
+# Return: none
+# translate each word in file, stores in a list, then outputs each word on its own line in a file, name chosen by user
 def translate_morse(translation, input, output):
+    #calls the dictionary
     morse_dict = dictionary(translation)
+    #open file for reading
     with open(input, 'r') as file:
         morse_lines = file.readlines()
     t_lines = []
+    #stores each word in the list
     for line in morse_lines:
         translated_words = []
         for words in line.strip().split(' '):
-
+            #joins each letter into the word it is s apart of
             word = ''.join(morse_dict[letter][0]
             for letter in words.split())
             translated_words.append(word)
@@ -29,7 +50,10 @@ def translate_morse(translation, input, output):
     with open(output, 'w') as outfile:
         outfile.write('\n'.join(t_lines) + '\n')
 
-
+# Name: read_file_name
+# Parameters: None
+# Return: f_name
+# Processes user's input and output's if user's input is invalid
 def read_file_name():
     f_name = input("Enter file name, (first is to translate dictionary, second is to translate a file): ")
     while not os.path.isfile(f_name):
@@ -37,11 +61,15 @@ def read_file_name():
     return f_name
 
 
-
+# Name: main
+# Parameters: None
+# Return: none
+# runs the main function
 def main():
     conversion_file = read_file_name()
     input_file = read_file_name()
     output_file = input("Enter the name of the output file: ")
     translate_morse(conversion_file, input_file, output_file)
 
+#calls main
 main()
